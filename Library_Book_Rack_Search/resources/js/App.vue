30cd104af447659d31f9f8445c4f449decae955e
Leo Chong <leochong0108@gmail.com>
2024-10-28 11:48:12 +0800

    connect vue with laravel

---
+++ Library_Book_Rack_Search/resources/js/App.vue
@@ -0,0 +1,17 @@
+<!-- resources/js/App.vue -->
+<template>
+    <h1>TESTING</h1>
+    <div id="app">
+        <router-view></router-view> <!-- Where the routed components will be displayed -->
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+};
+</script>
+
+<style>
+/* Add some global styles here if needed */
+</style>
--- Library_Book_Rack_Search/resources/js/app.js
+++ Library_Book_Rack_Search/resources/js/app.js
@@ -1,1 +1,10 @@
 import './bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
+
