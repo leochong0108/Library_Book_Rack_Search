a6473845694f5d0a4ddc0a100f113088c8be1a25
LooJiaChen0908 <jiachenloo@gmail.com>
2024-10-29 12:50:48 +0800

    install sweetalert

--- Library_Book_Rack_Search/resources/js/app.js
+++ Library_Book_Rack_Search/resources/js/app.js
@@ -3,8 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import Swal from 'sweetalert2';
 
 const app = createApp(App);
+
+app.config.globalProperties.$swal = Swal;
+
 app.use(router);
 app.mount('#app');
 
---
+++ Library_Book_Rack_Search/resources/js/components/sweetalert.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <button @click="showAlert" class="btn btn-primary">Show Alert</button>      
+    </div>
+  </template>
+  
+  <script>
+  import { ref, onMounted, getCurrentInstance } from 'vue';
+  import axios from 'axios';
+  
+  export default {
+    setup() {
+      const { appContext } = getCurrentInstance();
+
+      const showAlert = () => {
+        appContext.config.globalProperties.$swal.fire({
+            title: 'Are you sure?',
+            text: 'You won’t be able to revert this!',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#3085d6',
+            confirmButtonText: 'Yes',
+            cancelButtonText: 'Cancel'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // do something
+            }
+        });
+        };
+  
+      return { showAlert };
+    }
+  };
+  </script>
