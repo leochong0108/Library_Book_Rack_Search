c82f001d6f6dbd47d96b78d6d3bdf2cfaf9a4bbc
Leo Chong <leochong0108@gmail.com>
2024-11-06 13:56:29 +0800

    defined model fillable

--- Library_Book_Rack_Search/app/Models/Book_Rack.php
+++ Library_Book_Rack_Search/app/Models/Book_Rack.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book_Rack extends Model
 {
+
+    protected $fillable = [
+        'rack_layer',
+        'floor',
+        'range',
+    ];
+
     public function books(){
         return $this->hasMany('App\Models\Book');
     }
