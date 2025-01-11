d555f975a057e6665bbf07894ca45670125ee030
LooJiaChen0908 <jiachenloo@gmail.com>
2025-01-11 11:29:35 +0800

    missing code

--- Library_Book_Rack_Search/app/Http/Controllers/BookRackController.php
+++ Library_Book_Rack_Search/app/Http/Controllers/BookRackController.php
@@ -48,6 +48,8 @@ class BookRackController extends Controller
             'rack_layer' => 'nullable',
             'floor' => 'nullable',
             'range' => 'nullable',
+            'start' => 'nullable',
+            'end' => 'nullable',
 
         ]);
 
